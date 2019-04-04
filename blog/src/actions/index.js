7fda89bb675573fbbc3852b3978876e5e0eeacd1
andrewmayer <andrew@levelupdesign.com>
2019-04-04 10:43:44 -0700

    completed blog app

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,37 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //action creators called within action creators must be manually dispatched
+  await dispatch(fetchPosts());
+
+  const userIds = new Set(getState().posts.map(post => post.userId));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //LODASH uniq/map method
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  //LODASH chain method
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
+};
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get("/posts");
-
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  const response = await jsonPlaceholder.get(`/posts`);
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// "memoized" version
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
--- blog/src/reducers/usersReducer.js
+++ blog/src/reducers/usersReducer.js
@@ -1,7 +1,8 @@
 export default (state = [], action) => {
   switch (action.type) {
-    case "FETCH_POSTS":
-      return action.payload;
+    case 'FETCH_USER':
+      const testing = [...state, action.payload];
+      return testing;
     default:
       return state;
   }
